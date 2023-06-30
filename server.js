f2ff0f05ee72c4200c175f274460cc909511cdc8
Ezgi <107739024+ezgi-c@users.noreply.github.com>
2023-06-29 23:19:31 -0700

    connected to openAI

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-'use strict';
-
+const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-const PORT = process.env.PORT || 3002;
+async function completion () {
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "Translate this into 1. French, 2. Spanish and 3. Japanese:\n\nWhat rooms do you have available?\n\n1.",
+            temperature: 0.3,
+            max_tokens: 100,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+          });
+          console.log(response.data.choices[0].text);
+        //   return response;
+    } catch (error) {
+        console.error('Completion error:', error);
+    }
+}
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+completion();
