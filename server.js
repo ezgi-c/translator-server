9819eff9c0cc3fff57a2be04bf20082f8226de2c
Ezgi <107739024+ezgi-c@users.noreply.github.com>
2023-06-29 22:21:54 -0700

    initializing server

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
